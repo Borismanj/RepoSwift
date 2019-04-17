bb866994b785770f4e78b75c841076e40772df34
Borismanj <48659555+Borismanj@users.noreply.github.com>
2019-04-18 00:10:17 +0300

    hw6

---
+++ HW6_boris/HW6_boris/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW6_boris
+//
+//  Created by Borash on 17.04.2019.
+//  Copyright © 2019 Borash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ hw1_boris/HW1per/HW1per/main.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+print("Вычисление процентов через 5лет")
+
+//Вводные данные
+let depo = 100000
+let perc = 8
+
+//Формула
+var sum = depo * perc
+
+//Вывод
+print("Сумма депозита составит: \(sum)")
