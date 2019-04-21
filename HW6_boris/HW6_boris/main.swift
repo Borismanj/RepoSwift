8fd91a7d15511fb909fc8a5dc8c91f8abd3e456c
Borismanj <48659555+Borismanj@users.noreply.github.com>
2019-04-21 23:13:47 +0300

    hw7

--- HW6_boris/HW6_boris/main.swift
+++ HW6_boris/HW6_boris/main.swift
@@ -1,12 +1,35 @@
-//
-//  main.swift
-//  HW6_boris
-//
-//  Created by Borash on 17.04.2019.
-//  Copyright © 2019 Borash. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+struct bookCrossing {
+    var storeID: Int
+    var donaters: Int
+    var books: Int
+
+    init(storeID: Int, donaters: Int, books: Int) {
+        self.books = books
+        self.donaters = donaters
+        self.storeID = storeID
+    }
+}
+
+var booksArray: [bookCrossing] = [bookCrossing(storeID: 1, donaters: 13, books: 75),
+                                  bookCrossing(storeID: 2, donaters: 64,books: 90),
+                                  bookCrossing(storeID: 3, donaters: 3,books: 12),
+                                  bookCrossing(storeID: 4, donaters: 23,books: 44),
+                                  bookCrossing(storeID: 5, donaters: 8,books: 65)]
+
+
+var store = booksArray.map { $0.storeID }
+print("Открыто -  \(store.count) точек приема книг")
+
+var total = booksArray.map { $0.books }
+print("Количество книг: \(total)")
+
+// как к подобной схеме извлечения данных из коллекции можно применить стандарные операции? например просуммировать массив
+
+var allinfo = booksArray.compactMap { $0 }
+print(allinfo)
+
+
 
---
+++ HW7_boris/HW7_boris/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW7_boris
+//
+//  Created by Borash on 21.04.2019.
+//  Copyright © 2019 Borash. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
